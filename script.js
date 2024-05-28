5761c2346a8b12ad7d13142a65fd9bfc386b426e
Rahimatbi Jamadar <Rahimatbi.J@probuddy.corp>
2024-05-28 14:28:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const var1=React.createElement("div",{id:"parent"}
+,[[React.createElement("h1",{key:"child1"},"child1")]
+,[React.createElement("div",{key:"child2"},"child2")]])
+const pari=ReactDOM.createRoot(document.getElementById("demo"));
+pari.render(var1);
